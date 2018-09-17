c0e044de22b72eb52e78cf60da9bd865c13f991a
Paul Robinson <probi@AsusLaptop.its.ie>
2018-09-17 14:33:55 +0100

    added test cases

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -112,3 +112,43 @@ describe('GET /todos:id',()=>{
 
 });
 
+describe('DELETE /todos:id',()=>{
+    it(' : should remove a todo',(done)=>{
+        var hexId = testTodos[1]._id.toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res)=>{
+                if(err){
+                    return done(err);
+                }
+
+                Todo.findById(hexId)
+                    .then((todo)=>{
+                        expect(todo).toNotExist();
+                        done();
+                    }).catch((e)=>done(e));
+            });
+
+    });
+
+    it(' : should return 404 if todo not found',(done)=>{
+        var id = new ObjectID().toHexString();
+        request(app)
+            .delete(`/todos/${id}`)  
+            .expect(404)
+            .end(done);
+    });
+
+    it(' : shoudl return 404 if object id is invalid', (done)=>{
+        var id = new ObjectID().toHexString();
+        request(app)
+            .delete(`/todos/4556`)  
+            .expect(404)
+            .end(done);
+    });
+});
+
