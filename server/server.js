94c153f7010e36d3385cd5fdee924c38d8226047
Paul Robinson <probi@AsusLaptop.its.ie>
2018-09-17 19:02:16 +0100

    added private routes

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+var {authenticate} = require('./middleware/authenticate');
+
 var app = express();
 const port = process.env.PORT || 3000;
 
@@ -120,6 +122,12 @@ app.post('/users', (req, res)=>{
         .catch((err)=>{
             res.status(400).send(err);
         });
+});
+
+
+
+app.get('/users/me', authenticate, (req, res)=>{
+    res.send(req.user);
 });
 
 
