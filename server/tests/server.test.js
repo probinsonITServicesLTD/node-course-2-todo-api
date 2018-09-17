fff75a3f6cabb2c562581cabfbd48f98fdf989eb
Paul Robinson <probi@AsusLaptop.its.ie>
2018-09-17 10:56:54 +0100

    add test for GET todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,13 +4,26 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+
+const testTodos =  [
+    {
+        text:'First text todo'
+    },
+    {
+        text:'Second test todo'
+    }
+];
+
 beforeEach((done)=>{
     Todo.remove({})
-        .then(()=>done())
-})
+        .then(()=>{
+            return Todo.insertMany(testTodos);
+        })
+        .then(()=>done());
+});
 
 describe('POST /todos',()=>{
-    it('should create a new todo',(done)=>{
+    it(' : should create a new todo',(done)=>{
         var text = "Test todo text";
 
         request(app)
@@ -27,15 +40,15 @@ describe('POST /todos',()=>{
                     return done(err);
                 }
 
-                Todo.find().then((todos)=>{
+                Todo.find({text:text}).then((todos)=>{
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
                 }).catch((e)=>done(e))
-            })
+            });
     })
 
-    it('should not create todo with invalid body data', (done)=>{
+    it(' : should not create todo with invalid body data', (done)=>{
         request(app)
             .post('/todos')
             .send({})
@@ -46,10 +59,23 @@ describe('POST /todos',()=>{
                 }
 
                 Todo.find().then((todos)=>{
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e)=>done(e))
             })
+    })
+})
+
+describe('GET /todos',()=>{
+    it(' : should get all todos', (done)=>{
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+
     })
 })
 
